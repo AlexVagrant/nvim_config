93594db38452c44073f77ac8b360b8089a1195bb
wh.huajieyu <wang.shuang_202000@hellogroup.com>
2025-07-16 10:47:20 +0800

    feat(which-key): hide 'v' keybinding from which-key hints

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,6 +4,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("which-key").setup({
+      spec = { { "v", hidden = true } },
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
