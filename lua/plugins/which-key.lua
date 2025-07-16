98f59955b3cfc9e49f852562bbf303059149ab49
wh.huajieyu <wang.shuang_202000@hellogroup.com>
2025-07-16 10:58:21 +0800

    feat: Hide which-key menu in visual mode
    Configured which-key to ignore the 'v' key in visual mode, preventing the menu from
    popping up when entering visual mode.

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,6 +5,7 @@ return {
   config = function()
     require("which-key").setup({
       spec = { { "v", hidden = true } },
+      ignore_modes = { "v" },
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
