9ed2f094d032b19e051180d34c03d5033d6cee6d
wh.huajieyu <wang.shuang_202000@hellogroup.com>
2025-11-19 10:45:15 +0800

    refactor(leap): enhance leap.nvim configuration by organizing options and adding cross-window key mapping

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,10 +2,15 @@ return {
   {
     "ggandor/leap.nvim",
     config = function()
-      require('leap').add_default_mappings()
-      require('leap').opts.highlight_unlabeled_phase_one_targets = true
+      local leap = require('leap')
+      
+      -- 设置选项
+      leap.opts.highlight_unlabeled_phase_one_targets = true
+      
+      -- 设置默认键映射
       vim.keymap.set({'x', 'o', 'n'}, 's', '<Plug>(leap-forward-to)', {desc = 'Leap向前'})
       vim.keymap.set({'x', 'o', 'n'}, 'S', '<Plug>(leap-backward-to)', {desc = 'Leap向后'})
+      vim.keymap.set({'x', 'o', 'n'}, 'gs', '<Plug>(leap-from-window)', {desc = 'Leap跨窗口'})
     end
   },
   {
