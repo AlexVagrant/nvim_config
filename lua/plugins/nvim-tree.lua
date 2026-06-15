acc9f9f6455d6de29eb8d636fff4a1f76d3c5cd7
wh.huajieyu <wang.shuang_202000@hellogroup.com>
2026-06-14 23:46:39 -0400

    chore(nvim-tree): remove deprecated system_open config
    nvim-tree 已移除 system_open，改用 Neovim 内置的 vim.ui.open() 作为默认系统文件打开器

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -98,10 +98,7 @@ return {
         update_cwd = false,
         ignore_list = {},
       },
-      system_open = {
-        cmd = "",
-        args = {},
-      },
+
       diagnostics = {
         enable = false,
         show_on_dirs = false,
